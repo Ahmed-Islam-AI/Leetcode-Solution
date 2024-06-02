c889337bde3ca69db85065a8b626d0fab10ddbe5
Ahmed Islam <152423826+Ahmed-Islam-AI@users.noreply.github.com>
2024-06-02 22:55:25 +0500

    Time: 168 ms (61.98%), Space: 20.7 MB (98.96%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,18 +1,9 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        Two pointer and swapping Technique 
-        """
-        n= len(s)
-        left = 0 
-        right = n-1
-        while left<right:
-            ch = s[left]
-            s[left]= s[right]
-            s[right] = ch
-            left += 1
-            right -= 1
+        i, j = 0, len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i, j = i + 1, j - 1
 
 
 
